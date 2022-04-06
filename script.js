53c0e7d9b5c69b333db1b0ca519a89fdf9085432
Olga Smirnova <smrnva.olga@gmail.com>
2022-04-06 14:24:19 +0300

    working with api (joke)

--- script.js
+++ script.js
@@ -23,11 +23,17 @@ const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate
 
 //Get Jokes from Joke API
 async function getJokes() {
+	let joke = '';
 	const apiURL = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
 	try {
 		const response = await fetch(apiURL);
 		const data = await response.json();
-		console.log(data);
+		if (data.setup) {
+			joke = `${data.setup} ... ${data.delivery}`;
+		} else {
+			joke = data.joke;
+		};
+		console.log(joke);
 	} catch (error){
 		//Catch errors
 		console.log('Oi vavoi!', error);
